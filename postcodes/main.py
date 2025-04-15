f3ff2c09754a7d4d6191dd5b6133b62879e40e4f
masi23 <mateuszzwierzchowski19@gmail.com>
2025-04-15 23:50:28 +0200

    deleted garbage comments

--- postcodes/main.py
+++ postcodes/main.py
@@ -5,7 +5,6 @@ data = pd.read_csv('German-Zip-Codes.csv', delimiter=';', dtype={'Plz' : str})
 lands = data['Bundesland'].unique()
 grouped = data.groupby('Bundesland')
 summary = pd.DataFrame(columns=['land', 'code_prefix', 'code_range'])
-# summary = {}
 for land in lands:
     group = grouped.get_group(land)
     prefixes = []
@@ -13,7 +12,6 @@ for land in lands:
         prefix = row['Plz'][:2]
         if prefix not in prefixes:
             prefixes.append(prefix)
-    # print(prefixes)
     codes_ranges = []
     for prefix in prefixes:
         range_left, range_right = 0, 0
@@ -25,12 +23,10 @@ for land in lands:
                 range_left = int(code)
             if int(code) > range_right or range_right == 0:
                 range_right = int(code)
-        # codes_ranges.append((range_left, range_right))
         if len(str(range_left)) < 5:
             range_left = f'0{range_left}'
         if len(str(range_right)) < 5:
             range_right = f'0{range_right}'
         summary_new_row = pd.DataFrame([{'land': land, 'code_prefix' : str(prefix), 'code_range' : f'{range_left}-{range_right}'}])
         summary = pd.concat([summary, summary_new_row], ignore_index=True)
-# print(summary)
 summary.to_csv('summary.csv', index=False)
