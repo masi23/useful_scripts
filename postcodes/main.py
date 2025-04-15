472809e964bd86de46c06281129d6a134049e980
masi23 <mateuszzwierzchowski19@gmail.com>
2025-04-15 23:47:45 +0200

    added postcodes project

---
+++ postcodes/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('German-Zip-Codes.csv', delimiter=';', dtype={'Plz' : str})
+lands = data['Bundesland'].unique()
+grouped = data.groupby('Bundesland')
+summary = pd.DataFrame(columns=['land', 'code_prefix', 'code_range'])
+# summary = {}
+for land in lands:
+    group = grouped.get_group(land)
+    prefixes = []
+    for index, row in group.iterrows():
+        prefix = row['Plz'][:2]
+        if prefix not in prefixes:
+            prefixes.append(prefix)
+    # print(prefixes)
+    codes_ranges = []
+    for prefix in prefixes:
+        range_left, range_right = 0, 0
+        for index, row in group.iterrows():
+            code = row['Plz']
+            if code[:2] != prefix:
+                continue
+            if int(code) < range_left or range_left == 0:
+                range_left = int(code)
+            if int(code) > range_right or range_right == 0:
+                range_right = int(code)
+        # codes_ranges.append((range_left, range_right))
+        if len(str(range_left)) < 5:
+            range_left = f'0{range_left}'
+        if len(str(range_right)) < 5:
+            range_right = f'0{range_right}'
+        summary_new_row = pd.DataFrame([{'land': land, 'code_prefix' : str(prefix), 'code_range' : f'{range_left}-{range_right}'}])
+        summary = pd.concat([summary, summary_new_row], ignore_index=True)
+# print(summary)
+summary.to_csv('summary.csv', index=False)
